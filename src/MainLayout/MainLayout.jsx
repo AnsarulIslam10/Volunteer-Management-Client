680f99cc5837f7ad1c5205a87fe79729dec6d5bb
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-21 21:46:20 +0600

    footer added

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar/Navbar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const MainLayout = () => {
-    return (
-        <div className='max-w-7xl px-2 mx-auto'>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div className="max-w-7xl px-2 mx-auto">
+      <Navbar></Navbar>
+      <div className="min-h-[calc(100vh-288px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
