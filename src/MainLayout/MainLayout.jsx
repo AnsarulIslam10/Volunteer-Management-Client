d44aebcf389afae89d8d045fbc9c8258e6f4aa34
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-22 09:18:11 +0600

    conditional navbar design

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 const MainLayout = () => {
   return (
     <div className="max-w-7xl px-2 mx-auto">
+      <Tooltip id="my-tooltip" className="z-10" />
       <Navbar></Navbar>
       <div className="min-h-[calc(100vh-288px)]">
         <Outlet></Outlet>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,35 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider";
 
 const Navbar = () => {
+  const { user, signOutUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    signOutUser();
+    navigate("/");
+  };
   const links = (
     <>
       <li>
-        <NavLink to={'/'}>Home</NavLink>
+        <NavLink to={"/"}>Home</NavLink>
       </li>
       <li>
-        <NavLink to={'/all-volunteer-need-post'}>All volunteer Need posts</NavLink>
+        <NavLink to={"/all-volunteer-need-post"}>
+          All volunteer Need posts
+        </NavLink>
       </li>
       <li>
         <details>
           <summary>Profile</summary>
-          <ul className="p-2 w-52">
+          <ul className="p-2 w-52 z-10 rounded-md bg-gray-200 backdrop-blur-sm">
             <li>
-              <NavLink to={'/add-volunteer-need-post'}>Add Volunteer need Post</NavLink>
+              <NavLink to={"/add-volunteer-need-post"}>
+                Add Volunteer need Post
+              </NavLink>
             </li>
             <li>
-              <NavLink to={'/manage-my-posts'}>Manage My Posts</NavLink>
+              <NavLink to={"/manage-my-posts"}>Manage My Posts</NavLink>
             </li>
           </ul>
         </details>
@@ -52,13 +63,46 @@ const Navbar = () => {
             {links}
           </ul>
         </div>
-        <Link to={'/'} className="cursor-pointer text-3xl tracking-widest text-green-500 font-bold uppercase ">Vollify</Link>
+        <Link
+          to={"/"}
+          className="cursor-pointer text-3xl tracking-widest text-green-500 font-bold uppercase "
+        >
+          Vollify
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
-        <Link to={'/login'} className="btn bg-green-500 text-white font-semibold text-lg">Login</Link>
+        {user && user?.email ? (
+          <div className="flex items-center">
+          <div
+            className="flex items-center cursor-pointer px-1"
+            data-tooltip-id="my-tooltip"
+            data-tooltip-place="bottom"
+            data-tooltip-content={user.displayName}
+          >
+            <img
+              className="sm:w-10 sm:h-10 w-7 h-7 border-2 border-cyan-500 rounded-full ml-1"
+              src={user?.photoURL}
+              alt=""
+            />
+          </div>
+          <button
+            onClick={handleLogOut}
+            className="btn bg-green-500 text-white font-semibold text-lg"
+          >
+            Log Out
+          </button>
+        </div>
+        ) : (
+          <Link
+            to={"/login"}
+            className="btn bg-green-500 text-white font-semibold text-lg"
+          >
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
