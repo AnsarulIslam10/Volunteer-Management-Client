e7345fca72ec29624684ce827a6e6c0303eed5ab
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-22 19:28:01 +0600

    view mode toggle between grid and table view

--- src/pages/AllVolunteerNeedPosts/AllVolunteerNeedPosts.jsx
+++ src/pages/AllVolunteerNeedPosts/AllVolunteerNeedPosts.jsx
@@ -1,12 +1,14 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import PostCard from "../../components/PostCard/PostCard";
-import { FaSearch } from "react-icons/fa";
+import { FaBars, FaSearch, FaTh } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { format } from "date-fns";
 
 const AllVolunteerNeedPosts = () => {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
-
+  const [viewMode, setViewMode] = useState("grid");
   useEffect(() => {
     const fetchAddPosts = async () => {
       const { data } = await axios.get(
@@ -17,6 +19,10 @@ const AllVolunteerNeedPosts = () => {
     fetchAddPosts();
   }, [search]);
 
+  const toggleView = () => {
+    setViewMode((prevMode) => (prevMode === "grid" ? "table" : "grid"));
+  };
+
   return (
     <div className="my-16">
       <div className="flex items-center justify-between">
@@ -26,16 +32,84 @@ const AllVolunteerNeedPosts = () => {
             {posts.length}
           </span>
         </h1>
-        <label className="input input-bordered flex items-center mb-4 gap-2">
-          <input type="text" onChange={e => setSearch(e.target.value)} value={search} className="grow" placeholder="Search" />
-          <FaSearch />
-        </label>
+        <div className="flex items-center gap-4">
+          <div className="flex justify-end text-3xl gap-3 mb-3 text-gray-600">
+            <button onClick={toggleView}>
+              <FaTh
+                className={`${viewMode === "grid" ? "text-blue-400" : ""}`}
+              />
+            </button>
+            <button onClick={toggleView}>
+              <FaBars
+                className={`${viewMode === "table" ? "text-blue-400" : ""}`}
+              />
+            </button>
+          </div>
+          <label className="input input-bordered flex items-center mb-4 gap-2">
+            <input
+              type="text"
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+              className="grow"
+              placeholder="Search"
+            />
+            <FaSearch />
+          </label>
+        </div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {posts.map((post) => (
-          <PostCard key={post._id} post={post}></PostCard>
-        ))}
-      </div>
+
+      {viewMode === "grid" ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {posts.map((post) => (
+            <PostCard key={post._id} post={post}></PostCard>
+          ))}
+        </div>
+      ) : (
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead className="bg-green-500 text-white text-base">
+              <tr>
+                <th>Thumbnail</th>
+                <th>Title</th>
+                <th>Category</th>
+                <th>Deadline</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.map((post) => (
+                <tr key={post._id} className="hover shadow-md">
+                  <th>
+                    <img
+                      className="w-52 h-32 rounded-lg object-cover"
+                      src={post.thumbnail}
+                      alt=""
+                    />
+                  </th>
+                  <td className="text-xs sm:text-base md:text-lg font-semibold">
+                    {post.title}
+                  </td>
+                  <td className="text-gray-500 font-semibold">
+                    {post.category}
+                  </td>
+                  <td className="text-gray-500">
+                    {format(new Date(post.deadline), "P")}
+                  </td>
+                  <td>
+                    <Link
+                      className="btn bg-green-500 text-white hover:bg-green-600 transition-colors"
+                      to={`/volunteer-need-post-details/${post._id}`}
+                    >
+                      View Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
