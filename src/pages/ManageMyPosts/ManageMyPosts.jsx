764d243d722c315eb887e98bd4a063c7cc86fc45
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-23 22:42:03 +0600

    cancel my request post

--- src/pages/ManageMyPosts/ManageMyPosts.jsx
+++ src/pages/ManageMyPosts/ManageMyPosts.jsx
@@ -6,6 +6,7 @@ import moment from "moment";
 import { Link } from "react-router-dom";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import Swal from "sweetalert2";
+import { FcCancel } from "react-icons/fc";
 
 const ManageMyPosts = () => {
   const { user } = useContext(AuthContext);
@@ -35,7 +36,9 @@ const ManageMyPosts = () => {
       try {
         setLoading(true);
         const { data } = await axios.get(
-          `${import.meta.env.VITE_API_URL}/my-request-posts?email=${user?.email}`
+          `${import.meta.env.VITE_API_URL}/my-request-posts?email=${
+            user?.email
+          }`
         );
         setMyRequestPosts(data);
       } catch (error) {
@@ -81,143 +84,184 @@ const ManageMyPosts = () => {
       }
     });
   };
+  const handleCancel = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const { data } = await axios.delete(
+          `${import.meta.env.VITE_API_URL}/my-request-post/${id}`
+        );
+        if (data.deletedCount > 0) {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your Post has been deleted.",
+            icon: "success",
+          });
+          const remainingRequestPosts = myRequestPosts.filter((post) => post._id !== id);
+          setMyRequestPosts(remainingRequestPosts);
+        }
+      } else {
+        Swal.fire({
+          title: "Error",
+          text: "Somthing went wrong, try again.",
+          icon: "error",
+        });
+      }
+    });
+  };
 
   if (loading) {
     return <Loading></Loading>;
   }
 
-  if (!myPosts || myPosts.length === 0) {
-    return (
-      <div className="flex min-h-[60vh] justify-center items-center">
-        <p className="text-3xl font-semibold text-red-400">No Post found.</p>
-      </div>
-    );
-  }
-
   return (
     <div className="my-16">
-      <div className="mb-16">
-        <div className="flex items-center flex-col md:flex-row justify-between">
+      {!myPosts || myPosts.length === 0 ? (
+        <div className="min-h-[60vh]">
           <h1 className="text-4xl text-green-600 inline-block shadow-lg p-3 drop-shadow-xl font-bold up border-l-8 border-green-500 pl-3 mb-8">
             My Posts
-            <span className="badge badge-success text-white badge-lg relative bottom-7 -right-7 drop-shadow-lg w-10 h-10">
-              {myPosts.length}
-            </span>
           </h1>
+          <p className="text-3xl font-semibold text-center text-red-400">
+            No Post found.
+          </p>
         </div>
-        <div className="overflow-x-auto">
-          <table className="table">
-            {/* head */}
-            <thead className="bg-green-500 text-white text-base">
-              <tr>
-                <th>Thumbnail</th>
-                <th>Title</th>
-                <th>Category</th>
-                <th>Deadline</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {myPosts.map((post) => (
-                <tr key={post._id} className="hover shadow-md">
-                  <th>
-                    <img
-                      className="w-52 h-32 rounded-lg object-cover"
-                      src={post.thumbnail}
-                      alt=""
-                    />
-                  </th>
-                  <td className="text-xs sm:text-base md:text-lg font-semibold">
-                    {post.title}
-                  </td>
-                  <td className="text-gray-500 font-semibold">
-                    {post.category}
-                  </td>
-                  <td className="text-gray-500">
-                    {moment(post.deadline).format("DD/MM/YYYY")}
-                  </td>
-                  <td className="text-2xl space-x-3">
-                    <Link
-                      to={`/update-my-post/${post._id}`}
-                      className="text-green-500 inline-block hover:scale-125 transition-all duration-300"
-                    >
-                      <FaEdit />
-                    </Link>
-                    <button
-                      onClick={() => handleDelete(post._id)}
-                      className="text-red-600 hover:text-red-700 hover:scale-125 transition-all duration-300"
-                    >
-                      <FaTrash />
-                    </button>
-                  </td>
+      ) : (
+        <div className="mb-16">
+          <div className="flex items-center flex-col md:flex-row justify-between">
+            <h1 className="text-4xl text-green-600 inline-block shadow-lg p-3 drop-shadow-xl font-bold up border-l-8 border-green-500 pl-3 mb-8">
+              My Posts
+              <span className="badge badge-success text-white badge-lg drop-shadow-lg w-10 h-10 relative bottom-2 ml-2">
+                {myPosts.length}
+              </span>
+            </h1>
+          </div>
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead className="bg-green-500 text-white text-base">
+                <tr>
+                  <th>Thumbnail</th>
+                  <th>Title</th>
+                  <th>Category</th>
+                  <th>Deadline</th>
+                  <th>Action</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {myPosts.map((post) => (
+                  <tr key={post._id} className="hover shadow-md border">
+                    <th>
+                      <img
+                        className="w-52 h-32 rounded-lg object-cover"
+                        src={post.thumbnail}
+                        alt=""
+                      />
+                    </th>
+                    <td className="text-xs sm:text-base md:text-lg font-semibold">
+                      {post.title}
+                    </td>
+                    <td className="text-gray-500 font-semibold">
+                      {post.category}
+                    </td>
+                    <td className="text-gray-500">
+                      {moment(post.deadline).format("DD/MM/YYYY")}
+                    </td>
+                    <td className="text-2xl space-x-3">
+                      <Link
+                        to={`/update-my-post/${post._id}`}
+                        className="text-green-500 inline-block hover:scale-125 transition-all duration-300"
+                      >
+                        <FaEdit />
+                      </Link>
+                      <button
+                        onClick={() => handleDelete(post._id)}
+                        className="text-red-600 hover:text-red-700 hover:scale-125 transition-all duration-300"
+                      >
+                        <FaTrash />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-<div className="divider mb-16"></div>
-    {/* My post request */}
-    <div>
-        <div className="flex items-center flex-col md:flex-row justify-between">
+      )}
+
+      <div className="divider mb-16"></div>
+      {/* My post request */}
+      {!myRequestPosts || myRequestPosts.length === 0 ? (
+        <div className="min-h-[60vh]">
           <h1 className="text-4xl text-green-600 inline-block shadow-lg p-3 drop-shadow-xl font-bold up border-l-8 border-green-500 pl-3 mb-8">
-          My Volunteer Request Post
-            <span className="badge badge-success text-white badge-lg relative bottom-7 -right-7 drop-shadow-lg w-10 h-10">
-              {myRequestPosts.length}
-            </span>
+            My Volunteer Request Post
           </h1>
+          <p className="text-3xl font-semibold text-center text-red-400">
+            No Post found.
+          </p>
         </div>
-        <div className="overflow-x-auto">
-          <table className="table">
-            {/* head */}
-            <thead className="bg-green-500 text-white text-base">
-              <tr>
-                <th>Thumbnail</th>
-                <th>Title</th>
-                <th>Category</th>
-                <th>Deadline</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {myRequestPosts.map((post) => (
-                <tr key={post._id} className="hover shadow-md">
-                  <th>
-                    <img
-                      className="w-52 h-32 rounded-lg object-cover"
-                      src={post.thumbnail}
-                      alt=""
-                    />
-                  </th>
-                  <td className="text-xs sm:text-base md:text-lg font-semibold">
-                    {post.title}
-                  </td>
-                  <td className="text-gray-500 font-semibold">
-                    {post.category}
-                  </td>
-                  <td className="text-gray-500">
-                    {moment(post.deadline).format("DD/MM/YYYY")}
-                  </td>
-                  <td className="text-2xl space-x-3">
-                    <Link
-                      to={`/update-my-post/${post._id}`}
-                      className="text-green-500 inline-block hover:scale-125 transition-all duration-300"
-                    >
-                      <FaEdit />
-                    </Link>
-                    <button
-                      onClick={() => handleDelete(post._id)}
-                      className="text-red-600 hover:text-red-700 hover:scale-125 transition-all duration-300"
-                    >
-                      <FaTrash />
-                    </button>
-                  </td>
+      ) : (
+        <div>
+          <div className="flex items-center flex-col md:flex-row justify-between">
+            <h1 className="text-4xl text-green-600 inline-block shadow-lg p-3 drop-shadow-xl font-bold up border-l-8 border-green-500 pl-3 mb-8">
+              My Volunteer Request Post
+              <span className="badge badge-success text-white badge-lg drop-shadow-lg w-10 h-10 relative bottom-2 ml-2">
+                {myRequestPosts.length}
+              </span>
+            </h1>
+          </div>
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead className="bg-green-500 text-white text-base">
+                <tr>
+                  <th>Thumbnail</th>
+                  <th>Title</th>
+                  <th>Category</th>
+                  <th>Deadline</th>
+                  <th>Action</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {myRequestPosts.map((post) => (
+                  <tr key={post._id} className="hover shadow-md border">
+                    <th>
+                      <img
+                        className="w-52 h-32 rounded-lg object-cover"
+                        src={post.thumbnail}
+                        alt=""
+                      />
+                    </th>
+                    <td className="text-xs sm:text-base md:text-lg font-semibold">
+                      {post.title}
+                    </td>
+                    <td className="text-gray-500 font-semibold">
+                      {post.category}
+                    </td>
+                    <td className="text-gray-500">
+                      {moment(post.deadline).format("DD/MM/YYYY")}
+                    </td>
+                    <td className="text-2xl space-x-3">
+                      <button
+                        onClick={() => handleCancel(post._id)}
+                        className="text-red-600 hover:text-red-700 hover:scale-125 transition-all duration-300"
+                      >
+                        <FcCancel />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
