2a595d10f201b47883913ce9780a92e534f37472
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-25 23:54:55 +0600

    footer design fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="dark:bg-[#1d1d1d] bg-gray-100 py-10 sm:py-16 md:py-20 dark:text-[#e0e0e0] p-6 sm:p-10">
-      <footer className="footer max-w-7xl mx-auto px-2 p-10">
+    <footer className="dark:bg-[#1d1d1d] bg-gray-100 dark:text-[#e0e0e0]">
+      <footer className="footer max-w-7xl items-center mx-auto px-4 p-10">
         <aside className="flex flex-col justify-center items-center">
           <FaHands className="text-7xl text-green-500" />
           <p className="font-bold uppercase tracking-widest text-green-500 text-4xl">
--- src/pages/VolunteerNeedPostDetails/VolunteerNeedPostDetails.jsx
+++ src/pages/VolunteerNeedPostDetails/VolunteerNeedPostDetails.jsx
@@ -47,45 +47,51 @@ const VolunteerNeedPostDetails = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 text-lg mt-4">
           <div>
-            <p className="font-semibold text-gray-700">
+            <p className="font-semibold dark:text-[#e0e0e0] text-gray-700">
               Category:{" "}
-              <span className="font-normal text-gray-600">{category}</span>
+              <span className="font-normal dark:text-[#c0bebe]  text-gray-600">
+                {category}
+              </span>
             </p>
-            <p className="font-semibold text-gray-700">
+            <p className="font-semibold dark:text-[#e0e0e0] text-gray-700">
               Location:{" "}
-              <span className="font-normal text-gray-600">{location}</span>
+              <span className="font-normal dark:text-[#c0bebe]  text-gray-600">
+                {location}
+              </span>
             </p>
             {volunteersNumber <= 0 ? (
               <p className="text-lg text-green-500">Recruitment Closed</p>
             ) : (
-              <p className="font-semibold text-gray-700">
+              <p className="font-semibold dark:text-[#e0e0e0] text-gray-700">
                 Volunteers Neede:{" "}
-                <span className="font-normal text-gray-600">
+                <span className="font-normal text-green-500">
                   {volunteersNumber}
                 </span>
               </p>
             )}
 
-            <p className="font-semibold text-gray-700 mb-4">
+            <p className="font-semibold dark:text-[#e0e0e0] text-gray-700 mb-4">
               Deadline:{" "}
-              <span className="font-normal text-gray-600">
+              <span className="font-normal dark:text-[#c0bebe]  text-gray-600">
                 {moment(deadline).format("DD/MM/YYYY")}
               </span>
             </p>
           </div>
 
           <div>
-            <h3 className="text-gray-700 font-semibold">Organizers’s info:</h3>
+            <h3 className="text-gray-700 dark:text-[#e0e0e0] font-semibold">
+              Organizers’s info:
+            </h3>
             <div className="p-2 mb-2">
-              <p className="font-semibold text-gray-700">
+              <p className="font-semibold dark:text-[#e0e0e0] text-gray-700">
                 Name:
-                <span className="font-normal ml-1 text-gray-600">
+                <span className="font-normal dark:text-[#c0bebe]  ml-1 text-gray-600">
                   {organizer?.organizerName}
                 </span>
               </p>
-              <p className="font-semibold text-gray-700">
+              <p className="font-semibold dark:text-[#e0e0e0] text-gray-700">
                 Email:
-                <span className="font-normal ml-1 text-gray-600">
+                <span className="font-normal dark:text-[#c0bebe]  ml-1 text-gray-600">
                   {organizer?.organizerEmail}
                 </span>
               </p>
@@ -101,7 +107,7 @@ const VolunteerNeedPostDetails = () => {
           disabled={volunteersNumber <= 0}
           className="btn border-none mt-6 w-full bg-green-500 text-white hover:bg-green-600 transition-all duration-300"
         >
-          {volunteersNumber <= 0 ? 'Requretment Closed' : "Be a Volunteer"}
+          {volunteersNumber <= 0 ? "Requretment Closed" : "Be a Volunteer"}
         </button>
       </div>
 
